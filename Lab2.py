b44b73ab72b8554beefaeb27eeb596b75a9f77f2
Allan <phonethant.25@ichat.sp.edu.sg>
2026-05-05 12:32:38 +0800

    bmi.py has been made to return specific values based on the predetermined cases

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,38 @@
-print ("ET0735 (Devops for AIoT) - Lab 2 - Introduction to Python")
+def displya_main_menu():
+    print("display_main_menu")
+
+def get_user_input():
+    user_input = input()
+    string_list = user_input.split(",")
+
+    float_list = []
+    for item in string_list:
+        float_list.append(float(item))
+
+    return float_list
+
+def calculate_average_temperature(float_list):
+    total = sum(float_list)
+    average = total / len(float_list)
+    return average
+
+def calc_min_max_temperature(float_list):
+    minimum = min(float_list)
+    maximum = max(float_list)
+    return [minimum, maximum]
+
+def calc_median_temperature(float_list):
+    sorted_list = sorted(float_list)
+    n = len(sorted_list)
+
+    # if odd number of elements
+    if n % 2 == 1:
+        return sorted_list[n // 2]
+
+    # if even number of elements
+    mid1 = sorted_list[n // 2 - 1]
+    mid2 = sorted_list[n // 2]
+    return (mid1 + mid2) / 2
+
+def calc_average():
+    print("calc_average")
--- bmi.py
+++ bmi.py
@@ -6,4 +6,13 @@ def calculate_bmi(height, weight):
     bmi = weight / (height ** 2)
     print("Your bmi is:" + str(bmi))
 
+    if bmi < 18.5:
+        print("You are under weight")
+    elif bmi <= 35.0:
+        print("Your are normal weight")
+    else:
+        print("You are overweight")
+
+
+
 calculate_bmi(weight=57, height=1.73)
