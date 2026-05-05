cee6ba0fa98f534e7df774d988de09b7275a50e7
Allan <phonethant.25@ichat.sp.edu.sg>
2026-05-05 12:45:19 +0800

    Changes has been made to border values

--- bmi.py
+++ bmi.py
@@ -8,13 +8,13 @@ def calculate_bmi(height, weight):
 
     if bmi < 18.5:
         print("You are under weight")
-        return -1
-    elif bmi <= 35.0:
+        return bmi, -1
+    elif bmi <= 25.0:
         print("Your are normal weight")
-        return 0
+        return bmi, 0
     else:
         print("You are overweight")
-        return 1
+        return bmi, 1
 
 
 calculate_bmi(weight=57, height=1.73)
